0d63193050432516f6b6672d83bdbc0e2d7aaa91
Georgi Spasov <georgi.spasov@outlook.com>
2017-10-19 18:26:55 +0300

    Resolved

---
+++ Models/Contracts/IOffice.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Models.Contracts
+{
+    public interface IOffice
+    {
+        string City { get; set; }
+
+        string Address { get; set; }
+
+        ICollection<Car> Cars { get; set; }
+    }
+}
